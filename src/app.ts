3a813593f3989eabde2810638d4a0288596b2c6f
Igor Turko <igor@sharkpunch.com>
2022-07-13 15:06:57 +0300

    - nodecommand added; - reading port from the env; - gitignore updated;

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,14 @@ const app = fastify({
   logger: true,
 });
 
+const { PORT = 3000 } = process.env;
+
 const start = async () => {
   try {
+    const port: number = typeof PORT === 'number' ? PORT : parseInt(PORT, 10);
+
     app.register(setup);
-    await app.listen({ port: 3000 });
+    await app.listen({ port });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
